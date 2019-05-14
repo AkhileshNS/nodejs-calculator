f4913dbb3474253cb5578703ddf27c22eeaf5fe5
AkhileshNS <nsakhilesh02@gmail.com>
2019-05-14 09:29:12 +0530

    Switched to HTML serving

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const path = require('path');
 
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-
-app.post("/", (req, res) => {
-    const result = eval(req.body.operation);
-    res.send({result});
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./www/index.html"));
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
